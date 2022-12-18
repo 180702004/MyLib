d49e3be7ffa4ba33f80675d9744c2f046c2b0c74
U18070200401 <fatihemreyildiz@hotmail.com>
2022-12-18 22:42:36 +0300

    Home Activity added.

--- app/src/main/java/com/example/mylib/HomeActivity.java
+++ app/src/main/java/com/example/mylib/HomeActivity.java
@@ -1,14 +1,41 @@
 package com.example.mylib;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        listView = (ListView)findViewById(R.id.myListView);
+
+        // This should be fetched from firebase
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("MSKU");
+        arrayList.add("Belediye");
+        arrayList.add("BookStore");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(HomeActivity.this,"Clicked on" + arrayList.get(i),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mylib/MainActivity.java
+++ app/src/main/java/com/example/mylib/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.mylib;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.SplashScreen);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent dsp = new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(dsp);
+            finish();
+        },2500);
+
     }
 }
