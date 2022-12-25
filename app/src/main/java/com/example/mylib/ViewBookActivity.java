8f983a6a3cc40da633a84bee12a1f88e27810607
aylinduran <aylinduran@outlook.com.tr>
2022-12-25 13:33:31 +0300

    View book activity updated

--- app/src/main/java/com/example/mylib/ViewBookActivity.java
+++ app/src/main/java/com/example/mylib/ViewBookActivity.java
@@ -2,9 +2,7 @@ package com.example.mylib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class ViewBookActivity extends AppCompatActivity {
 
@@ -12,11 +10,5 @@ public class ViewBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_book);
-
-
-        Activity listItemView = null;
-        ImageView imgView = listItemView.findViewById(R.id.photoInfo);
-        imgView.setImageResource(R.drawable.book_default_image);
-
     }
 }
